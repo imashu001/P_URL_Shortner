624f0d1c5a577ce2eb13559ab5fadbcae784e388
Ashutosh kumar <imashu001@gmail.com>
2024-07-06 03:10:16 +0530

    adding the logic to redirect the short url and the next step as comment in the file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # 1. Library imports
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 
 # 2. create app object
@@ -10,6 +11,16 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'message': 'API working connection'}
+
+# write a route to save and map short url to long one in the Database
+
+@app.get('/shorturl')
+def redirector():
+    # this function can be taking the shorturl as the parameter and finding the corrosponding long url to this short url and than redirect to the long url
+    # here the status code is important without providing the correct status code redirection wont happen
+    return RedirectResponse(
+        url="https://api.slingacademy.com", status_code=302
+    )
 
 if __name__ == "__main__":
     uvicorn.run(app,host='127.0.0.1',port=1000)
